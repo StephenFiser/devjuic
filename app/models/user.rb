ecf2d51593b00af61727ef84b7816d61cd7c421e
Stephen Fiser <stephen.fiser@gmail.com>
2013-09-22 18:55:28 -0500

    added style to blog show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   # Unused Modules:
-	  # :token_authenticatable, :confirmable, :registerable,
+	  # :token_authenticatable, :confirmable
 	  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, 
+  devise :database_authenticatable, , :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   attr_accessible :first_name, :last_name, :email, :password, :password_confirmation, :remember_me
