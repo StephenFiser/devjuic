acff2fa24e4304cf005fc35d1c8e9edd39b5e998
Stephen Fiser <stephen.fiser@gmail.com>
2013-09-22 18:59:53 -0500

    updated config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,5 +64,5 @@ Devjuic::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.action_mailer.default_url_options = { :host => 'http://www.devjuic.com' }
+  config.action_mailer.default_url_options = { :host => 'http://tranquil-ravine-5622.herokuapp.com' }
 end
