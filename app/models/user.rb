1a7a82fdede297473826c900c0a8f2b47cb9afa0
Stephen Fiser <stephen.fiser@gmail.com>
2013-09-22 19:09:48 -0500

    removed registerable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   # Unused Modules:
-	  # :token_authenticatable, :confirmable
+	  # :token_authenticatable, :confirmable, :registerable,
 	  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, 
          :recoverable, :rememberable, :trackable, :validatable
 
   attr_accessible :first_name, :last_name, :email, :password, :password_confirmation, :remember_me
