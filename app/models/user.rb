a679ae35ccb294b650cf8b25cda8ace7975452ab
Stephen Fiser <stephen.fiser@gmail.com>
2013-09-22 19:01:52 -0500

    removed extra comma

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Unused Modules:
 	  # :token_authenticatable, :confirmable
 	  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, , :registerable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   attr_accessible :first_name, :last_name, :email, :password, :password_confirmation, :remember_me
